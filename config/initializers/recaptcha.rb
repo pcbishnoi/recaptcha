6996478fed2c4dd57c2c91edf403e6ceac194817
Poonma Bishnoi <shauspc@gmail.com>
2015-05-20 11:57:36 +0000

    first commit of recaptcha

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,9 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Le0IwcTAAAAAA_RVrH8fhwHnP8kpiZqXcRgpqXl'
+  config.private_key = '6Le0IwcTAAAAAP32Ia5UxXjAgzvzUhQEKn2y-nD6'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+  # Uncomment if you want to use the newer version of the API,
+  # only works for versions >= 0.3.7:
+  # config.api_version = 'v2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captcha::Application.config.secret_token = '4dbd306cdd321604f0c87423e2e64f240dc05753ceadf3a37dbfed86bcffd8225503a4ad6514ad9ce8c1ddbaf71693d443c63a65e5ccfaeaa3296d805f2d8b23'
